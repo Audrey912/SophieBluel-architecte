551c641df5f84e24a1e66ea89157cd282a5b5a1b
Audrey912 <audreyleite.al@gmail.com>
2023-05-03 17:29:23 +0200

    modification 1 étape 1.1

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,13 +1,18 @@
-fetch("http://localhost:5678/api/works").then((response)=>response.json()).then(response=>console.log(response));
+async function works(){
+    const response=await fetch("http://localhost:5678/api/works")
+    const resultat=await response.json()
+    console.log(resultat);
+};
+works();
+
+const tablProjets=Object.keys(works);
+console.log(tablProjets);
 
 const gallery=document.querySelector("gallery");
 const projetsDiv=document.createElement("figure");
 const imgProjets=document.createElement("img");
 const titleProjets=document.createElement("figcaption");
-imgProjets.src=element.imageurl;
-titleProjets.innerHTML=element.title;
 gallery.appendChild(projetsDiv);
 projetsDiv.appendChild(imgProjets);
 projetsDiv.appendChild(titleProjets);
-
 
