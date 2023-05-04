98039397a56995a5f4700bef11ee3b55d6d019a3
Audrey912 <audreyleite.al@gmail.com>
2023-05-04 11:59:46 +0200

    modification 2 étape 1.1

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,19 +1,26 @@
-//appel de l'API/Get
+//appel de l'API Get/works
 async function works(){
     const response=await fetch("http://localhost:5678/api/works")
     const resultat=await response.json()
     console.log(resultat);
+    //création d'une constante rattachée à la class 'gallery'
+    const gallery=document.querySelector('gallery');
+    //boucle pour créer autant de balise que déléments du tableau
+    for(let i=0; i<resultat.length; i++){
+        console.log(resultat[i]);
+    }
+    //création des balises dans le HTML
+    const projetDiv=document.createElement('figure');
+    const imgProjet=document.createElement('img');
+    const titleProjet=document.createElement("figcaption");
+    //indication des éléments à récupérer dans le tableau et à afficher
+    imgProjet.src=i[imageUrl];
+    titleProjet.innerHTML=i[title];
+    //rattachement des enfants aux parents
+    gallery.appendChild(projetDiv);
+    projetDiv.appendChild(imgProjet);
+    projetDiv.appendChild(titleProjet);
 };
 works();
 
-//création d'une constante rattachée à la class 'gallery'
-const gallery=document.querySelector("gallery");
-//création des balises dans le HTML
-const projetDiv=document.createElement("figure");
-const imgProjet=document.createElement("img");
-const titleProjet=document.createElement("figcaption");
-//rattachement des enfants aux parents
-gallery.appendChild(projetDiv);
-projetDiv.appendChild(imgProjet);
-projetDiv.appendChild(titleProjet);
 
