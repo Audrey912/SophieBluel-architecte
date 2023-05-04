747ebb92c5d9c8e86358fa75bae93b52def3bc50
Audrey912 <audreyleite.al@gmail.com>
2023-05-04 17:09:28 +0200

    etape 1.1 validée + css button validé + début étape 1.2

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -4,25 +4,40 @@ async function works(){
     const resultat=await response.json()
     console.log(resultat);
     //création d'une constante rattachée à la class 'gallery'
-    const gallery=document.querySelector('gallery');
-    //déclarer la valeur de i
-    let i=0;
+    const gallery=document.querySelector('.gallery');
     //boucle pour créer autant de balise que déléments du tableau
     for(let i=0; i<resultat.length; i++){
         console.log(resultat[i]);
+        //création des balises dans le HTML
+        const projetDiv=document.createElement('figure');
+        const imgProjet=document.createElement('img');
+        const titleProjet=document.createElement("figcaption");
+        //indication des éléments à récupérer dans le tableau et à afficher
+        imgProjet.src=resultat[i].imageUrl;
+        titleProjet.innerHTML=resultat[i].title;
+        //rattachement des enfants aux parents
+        gallery.appendChild(projetDiv);
+        projetDiv.appendChild(imgProjet);
+        projetDiv.appendChild(titleProjet);
     }
-    //création des balises dans le HTML
-    const projetDiv=document.createElement('figure');
-    const imgProjet=document.createElement('img');
-    const titleProjet=document.createElement("figcaption");
-    //indication des éléments à récupérer dans le tableau et à afficher
-    imgProjet.src=resultat[i].imageUrl;
-    titleProjet.innerHTML=resultat[i].title;
-    //rattachement des enfants aux parents
-    gallery.appendChild(projetDiv);
-    projetDiv.appendChild(imgProjet);
-    projetDiv.appendChild(titleProjet);
 };
 works();
+
+//appel de l'API Get/categories
+async function categories(){
+    const response=await fetch('http://localhost:5678/api/categories')
+    const resultat=await response.json()
+    console.log(resultat);
+    const tous=document.createElement('button');
+    tous.innerHTML='Tous';
+    const contenerButton=document.querySelector('.contenerbutton');
+    contenerButton.appendChild(tous);
+    for(let i=0; i<resultat.length; i++){
+        const button=document.createElement('button');
+        button.innerHTML=resultat[i].name;
+        contenerButton.appendChild(button);
+    }
+};
+categories();
 
 
