eb7c6cce661f9292833b25480b495b772cfabf7f
Audrey912 <audreyleite.al@gmail.com>
2023-05-12 10:51:08 +0200

    authentification de l'utilisateur validée

--- FrontEnd/assets/loginScript.js
+++ FrontEnd/assets/loginScript.js
@@ -1,7 +1,14 @@
-//appel de l'API POST/login
-async function login(){
-    const response=await fetch("http://localhost:5678/api/users/login")
-    const resultat=await response.json()
-    console.log(resultat);
-};
-login();
+const email=document.querySelector('#email');
+const password=document.querySelector('#password');
+const button=document.querySelector('button');
+button.addEventListener('click', async function(){
+    const resultat= await fetch('http://localhost:5678/api/users/login',{
+        method:'POST',
+        headers:{ "Content-Type": "application/json" },
+        body:JSON.stringify({"email": email.value,
+            "password": password.value})
+})
+const response= await resultat.json();
+    console.log("retour",response);
+});
+
